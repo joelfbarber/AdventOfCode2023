8fcac7b7ba6281cc896d208c6f6e84b01da9f803
Joel Barber <BarberJ1@b2b.regn.net>
2024-02-26 09:13:15 -0500

    started day 3

---
+++ AdventOfCode/Day3/Day3.fs
@@ -0,0 +1,13 @@
+﻿module Day3
+
+open System.IO
+
+let parseText (text: string) : char[][] =
+    text.Split([|'\n'; '\r'|], System.StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun line -> line.ToCharArray())
+
+let parsedText = 
+    File.ReadAllText ".\Day3\input.txt"
+    |> parseText
+
+
